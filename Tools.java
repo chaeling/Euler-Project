b28415281c7fca7cbb7ac7a1055baf096e84f8c2
Fan Sun <mitcc@qq.com>
2013-05-08 00:14:19 +0800

    Create Tools.java

---
+++ Tools.java
@@ -0,0 +1,18 @@
+package number;
+
+public class Tools {
+  public static boolean isPrime(int n){
+		if(n < 10){
+			if(n == 2 || n == 3 || n == 5 || n == 7)
+				return true;
+		}else{
+			if(n%2 != 0){
+				for(int i = 3; i <= Math.sqrt(n); i += 2)
+					if(n%i == 0)
+						return false;
+				return true;
+			}
+		}
+		return false;
+	}
+}
