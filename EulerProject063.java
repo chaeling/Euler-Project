52a364ef314a3f7309ef6d854d0de84e703d2263
Fan Sun <mitcc@qq.com>
2013-05-14 22:41:37 +0800

    Update EulerProject063.java

--- EulerProject063.java
+++ EulerProject063.java
@@ -28,8 +28,8 @@ public class PowerfulDigitCounts {
 		System.out.println("Runtime is " + time/1000/1000.0 + " ms.");
 	}*/
 	public static void main(String[] args) {
-		long start = System.nanoTime();        // 10^(n-1) <= x^n < 10^n  => x < 10, that is x = 1, 2, 3, 4, 5, 6, 7, 8, 9.
-		int count = 0;                         // When x is 9, 10^(n-1) <= 9^n => n <= (int)(log10/(log10 - log9)) = 21.      
+		long start = System.nanoTime();  // 10^(n-1) <= x^n < 10^n  => x < 10, that is x = 1, 2, 3, 4, 5, 6, 7, 8, 9.
+		int count = 0;                   // When x is 9, 10^(n-1) <= 9^n => n <= (int)(log10/(log10 - log9)) = 21.      
 		for(int i = 1; i <= 9; i++)
 			count += (int)(Math.log(10)/(Math.log(10)-Math.log(i)));
 		System.out.println(count);
