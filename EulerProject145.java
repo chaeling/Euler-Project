ebf5d90f55d81bbf5a0c91b1bc94e8ec23ababa6
Fan Sun <mitcc@qq.com>
2013-05-24 23:57:19 +0800

    Create EulerProject145.java

---
+++ EulerProject145.java
@@ -0,0 +1,46 @@
+/*
+ * How many reversible numbers are there below one-billion?
+ * Problem 145
+ * 
+ * Some positive integers n have the property that the sum [ n + reverse(n) ] consists entirely of odd (decimal) 
+ * digits. For instance, 36 + 63 = 99 and 409 + 904 = 1313. We will call such numbers reversible; so 36, 63, 409, 
+ * and 904 are reversible. Leading zeroes are not allowed in either n or reverse(n).
+
+ * There are 120 reversible numbers below one-thousand.
+
+ * How many reversible numbers are there below one-billion (10^9)?
+ */
+package number;
+
+public class ReversibleNumbers {
+  private static boolean isReversible(long n){
+		if(n % 10 == 0)
+			return false;
+		int digit = 1;
+		String s = Long.toString(n);
+		int len = s.length();
+		for(int i = 0; i < len; i++){
+			digit = s.charAt(i) - '0' + s.charAt(len - 1 - i) - '0' + digit/10;
+			if(digit % 2 == 0)
+				return false;
+		}
+		return true;
+		/*while(i < len){
+			digit = s.charAt(i) - '0' + s.charAt(len - 1 - i) - '0' + digit/10;
+			if(digit % 2 == 0)
+				return false;
+			i++;
+		}
+		return true;*/
+	}
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		int count = 0;
+		for(int i = 0; i < 1000000000; i++)
+			if(isReversible(i))
+				count++;
+		System.out.println(count);
+		long time = System.nanoTime() - start;
+		System.out.println("Runtime is " + time/1000/1000.0 + " ms.");
+	}
+}
