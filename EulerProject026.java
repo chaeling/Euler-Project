8188cdb4ef84b70889a8cb5ca2200d92d29bba48
Fan Sun <mitcc@qq.com>
2013-06-10 01:02:46 +0800

    Update EulerProject026.java

--- EulerProject026.java
+++ EulerProject026.java
@@ -4,7 +4,7 @@
  * 
  * A unit fraction contains 1 in the numerator. The decimal representation of the 
  * unit fractions with denominators 2 to 10 are given:
- *                1/2  = 	0.5
+ *                1/2	= 	0.5
  *                1/3	= 	0.(3)
  *                1/4	= 	0.25
  *                1/5	= 	0.2
@@ -21,26 +21,43 @@
  */
 package number;
 
+import java.util.*;
+
 public class ReciprocalCycles {
-	public static boolean isPrime(int n){
-		if(n < 10){
-			if(n == 2 || n == 3 || n == 5 || n == 7)
-				return true;
-		}else{
-			if(n%2 != 0){
-				for(int i = 3; i <= Math.sqrt(n); i += 2)
-					if(n%i == 0)
-						return false;
-				return true;
-			}
+/*	public static int recurringLength(int n) {
+		List<Integer> list = new LinkedList<Integer>();
+		int temp = 10;
+		while(!list.contains(temp % n)) {
+			list.add(temp % n);
+			temp = temp % n * 10;
 		}
-		return false;
-	}
+		return list.size() - list.indexOf(temp % n);
+	}*/
 	public static void main(String[] args) {
-		int max = 0;
-		for(int i = 1; i < 1000; i++)
-			if(isPrime(i))
-				max = i;
-		System.out.println(max);
+		long start = System.nanoTime();
+/*		int count = 1;
+		for(int i = 1; i < 1000; i++) 
+			count = recurringLength(count) > recurringLength(i) ? count : i;
+		System.out.println(count);*/
+		
+		int len = 0;
+		for(int i = 1000; i > 1; i--) {
+			if(i <= len)
+				break;
+			int[] remaider = new int[i];
+			int position = 0, value = 1;
+			while(remaider[value] == 0 && value != 0) {
+				remaider[value] = position;
+				value *= 10;
+				value %= i;
+				position++;
+			}
+			if(position - remaider[value] > len)
+				len = position - remaider[value] + 1;
+		}
+		System.out.println(len);
+		
+		long time = System.nanoTime() - start;
+		System.out.println("Runtime is " + time/1000/1000.0 + " ms.");
 	}
 }
