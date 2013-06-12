581d38aa447d64f602ee95a2e2de1bc3132b15a9
Fan Sun <mitcc@qq.com>
2013-06-12 22:08:06 +0800

    Update EulerProject120.java

--- EulerProject120.java
+++ EulerProject120.java
@@ -2,7 +2,7 @@
  * Square remainders
  * Problem 120
  * 
- * Let r be the remainder when (a−1)^n + (a+1)^n is divided by a2.
+ * Let r be the remainder when (a−1)^n + (a+1)^n is divided by a^2.
 
  * For example, if a = 7 and n = 3, then r = 42: 6^3 + 8^3 = 728 ≡ 42 mod 49. And as n varies, so too 
  * will r, but for a = 7 it turns out that rmax = 42.
