696aca0ac50f95be0142d224982f4a3b81875b1d
Fan Sun <mitcc@qq.com>
2013-06-26 12:32:10 +0800

    Update EulerProject003.java

--- EulerProject003.java
+++ EulerProject003.java
@@ -7,21 +7,6 @@
 package number;
 
 public class LargestPrimeFactor {
-  /*private static boolean isPrime(long n){
-		if(n < 2)
-			return false;
-		else if(n == 2)
-			return true;
-		else if(n%2 == 0)
-			return false;
-		else{
-			for(long i = 2; i <= java.lang.Math.sqrt(n); i++)
-				if(n%i == 0)
-					return false;
-			return true;
-		}
-	}
-	*/
 	public static long largestPrimeFactor(long n){
 		int i = 2;
 		while(i != n){
