71aa373876f86d999b4ba24163fd07d91304a610
Fan Sun <mitcc@qq.com>
2013-07-13 15:12:58 +0800

    Update EulerProject104.java

--- EulerProject104.java
+++ EulerProject104.java
@@ -4,11 +4,12 @@
  The Fibonacci sequence is defined by the recurrence relation:
 
  Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
- It turns out that F541, which contains 113 digits, is the first Fibonacci number for which the last nine digits are 1-9 pandigital 
- (contain all the digits 1 to 9, but not necessarily in order). And F2749, which contains 575 digits, is the first Fibonacci number 
- for which the first nine digits are 1-9 pandigital.
+ It turns out that F541, which contains 113 digits, is the first Fibonacci number for which the last nine digits 
+ are 1-9 pandigital (contain all the digits 1 to 9, but not necessarily in order). And F2749, which contains 575 
+ digits, is the first Fibonacci number for which the first nine digits are 1-9 pandigital.
 
- Given that Fk is the first Fibonacci number for which the first nine digits AND the last nine digits are 1-9 pandigital, find k.
+ Given that Fk is the first Fibonacci number for which the first nine digits AND the last nine digits are 1-9 
+ pandigital, find k.
  */
 package number;
 
@@ -16,7 +17,7 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class PandigitalFibonacciEnds {
-  
+	
 	public static boolean isPandigital(int n) {
 		if(n < 123456789)
 			return false;
