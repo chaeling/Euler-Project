a2b554ae445e2b62d0e7aa9f4cced078edb1b890
Fan Sun <mitcc@qq.com>
2013-07-14 14:03:07 +0800

    Update EulerProject053.java

--- EulerProject053.java
+++ EulerProject053.java
@@ -10,7 +10,7 @@
 
  * In general,
 
- *        nCr =  n!/(r!(n−r)!), where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
+ *        nCr =	n!/(r!(n−r)!), where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
  * It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
 
  * How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are greater than one-million?
@@ -28,22 +28,27 @@ public class CombinatoricSelections {
 			value = value.multiply(BigInteger.valueOf(i));
 		return value;
 	}
+	
 	public static BigInteger combination(int m, int n){
 		if(m > n)
 			throw new IllegalArgumentException();
-		BigInteger a = factorial(m, n);
-		BigInteger b = factorial(m, m);
-		return a.divide(b);
+		return factorial(m, n).divide(factorial(m, m));
 	}
+	
 	public static void main(String[] args) {
 		long start = System.nanoTime();
+		
 		int count = 0;
 		for(int i = 1; i <= 100; i++)
-			for(int j = 0; j <= i; j++)
-				if(combination(j, i).compareTo(BigInteger.valueOf(1000000)) > 0)
-					count++;
+			if(combination(i / 2, i).compareTo(BigInteger.valueOf(1000000)) > 0) {
+				int j;
+				for(j = i / 2; combination(j, i).compareTo(BigInteger.valueOf(1000000)) > 0; j--);
+				j++;
+				count += i - 2 * j + 1;
+			}
 		System.out.println(count);
+		
 		long time = System.nanoTime() - start;
 		System.out.println("Runtime is " + time/1000/1000.0 + " ms.");
-	}
+	}	
 }
